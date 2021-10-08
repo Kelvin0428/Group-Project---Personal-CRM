c394843adba177a7a9948bc3a857aa96ab6e119d
frankwan41 <yicongw@student.unimelb.edu.au>
2021-10-08 19:44:22 +0800

    Add editEvent and deleteEvent function

--- routes/BusinessRouter.js
+++ routes/BusinessRouter.js
@@ -6,9 +6,10 @@ const BusinessController = require('../Controllers/BusinessController.js')
 const BusinessRouter = express.Router()
 BusinessRouter.get('/Binfo',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.getBusinessInfo(req,res))
 BusinessRouter.get('/events',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.viewEvents(req,res))
-BusinessRouter.get('/event/:_id',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.oneEvent(req,res))
 BusinessRouter.post('/createEvent',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.createEvent(req,res))
-
+BusinessRouter.get('/event/:_id',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.oneEvent(req,res))
+BusinessRouter.post('/event/edit/:_id',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.editEvent(req,res))
+BusinessRouter.get('/event/delete/:_id',passport.authenticate('Bjwt',{session: false}), (req,res) =>BusinessController.deleteEvent(req,res))
 
 
 
